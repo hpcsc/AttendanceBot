c44e9f6f710e06b860d4222eef1798483ad73a6a
David Nguyen <hungphuongjc@gmail.com>
2016-06-17 13:56:58 +0800

    add string extension tests

---
+++ AttendanceBot.Tests/Helpers/StringExtensionsTests.cs
@@ -0,0 +1,39 @@
+﻿using AttendanceBot.Helpers;
+using NUnit.Framework;
+
+namespace AttendanceBot.Tests.Helpers
+{
+    public class StringExtensionsTests
+    {
+        [TestFixture]
+        public class EqualsIgnoreCaseMethod
+        {
+            [Test]
+            public void CompareTwoSameStringsSameCase_ShouldReturnTrue()
+            {
+                var first = "Test String";
+                var second = "Test String";
+
+                Assert.IsTrue(StringExtensions.EqualsIgnoreCase(first, second));
+            }
+
+            [Test]
+            public void CompareTwoSameStringsDifferentCase_ShouldReturnTrue()
+            {
+                var first = "Test String";
+                var second = "test string";
+
+                Assert.IsTrue(StringExtensions.EqualsIgnoreCase(first, second));
+            }
+
+            [Test]
+            public void CompareTwoDifferentStrings_ShouldReturnFalse()
+            {
+                var first = "First String";
+                var second = "Second String";
+
+                Assert.IsFalse(StringExtensions.EqualsIgnoreCase(first, second));
+            }
+        }
+    }
+}

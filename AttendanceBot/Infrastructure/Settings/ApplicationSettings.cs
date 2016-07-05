5a58c7e41b2fe71624523f439bc35fef3d881e44
hpcsc <hungphuongjc@gmail.com>
2016-07-06 00:32:08 +0800

    use mongo for elmah store

--- AttendanceBot/Infrastructure/Settings/ApplicationSettings.cs
+++ AttendanceBot/Infrastructure/Settings/ApplicationSettings.cs
@@ -4,7 +4,7 @@ namespace AttendanceBot.Infrastructure.Settings
 {
     public class ApplicationSettings
     {
-        public string ConnectionString => ConfigurationManager.AppSettings["ConnectionString"];
+        public string ConnectionString => ConfigurationManager.ConnectionStrings["MainConnectionString"].ConnectionString;
         public string DatabaseName => ConfigurationManager.AppSettings["DatabaseName"];
     }
 }
